2893715972dfc855d65481375b2305ae0706149a
Facundo <facundolaffont@gmail.com>
2022-05-07 22:36:47 -0300

    Set data structure examples - Added Set.py.

---
+++ Data structures/Set.py
@@ -0,0 +1,14 @@
+mySet = {"element", 1.5, 50}
+
+# Print the entire set.
+print(mySet)
+
+# Print the length.
+print(len(mySet))
+
+# Print all elements.
+for i in mySet:
+    print(i)
+
+# Print the first element.
+print(list(mySet)[0])
